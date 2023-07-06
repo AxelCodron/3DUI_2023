922370c994482263fc1de65abfda09755d457aff
AxelCodron <codronaxel@gmail.com>
2023-07-06 16:48:24 +0200

    Finished forest and linked the house

---
+++ Assets/Scripts/EnterHouseCollider.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterHouseCollider : MonoBehaviour
+{
+    public GameObject text;
+    private void OnTriggerEnter(Collider other)
+    {
+        text.SetActive(true);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        text.SetActive(false);
+    }
+}
--- Assets/Scripts/SpiritCollider.cs
+++ Assets/Scripts/SpiritCollider.cs
@@ -8,9 +8,22 @@ public class SpiritCollider : MonoBehaviour
     public Transform target;
     public List<Vector3> positions;
     private int i = 0;
+    private int _security;
     private void OnTriggerEnter(Collider other)
     {
-        target.position = positions[i];
-        i++;
+        if (_security == 0)
+        {
+            target.position = new Vector3(positions[i].x, positions[i].y, positions[i].z);
+            Debug.Log("Target is" + positions[i].x + " ," + positions[i].y + ", " + positions[i].z);
+            i++;
+            _security++;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (_security == 1) {
+            _security = 0;
+        }
     }
 }
